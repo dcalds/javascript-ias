b8cce7fe4e9358accc737e925f6717d47fda5acf
Danilo Caldas <danilo-caldas-0000@hotmail.com>
2019-08-31 01:10:48 -0300

    Gerador de Seed Aleatória

--- client/src/components/body/Body.js
+++ client/src/components/body/Body.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Styles.css';
 
 function Body(){
+
+    // Hook que controla o estado da Seed
+    const [seed, setSeed] = useState([0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1])
+
+    // Função que gera uma seed aleatória
+    const generateSeed = () => {
+        var newSeed = []
+        for (var i = 0; i <= 39; i++){
+            newSeed.push(Math.floor(Math.random() * 2))
+        }
+        setSeed(newSeed)
+    }
+
     return(
         <div className="body">
-            0101101010010110101001011010100101101010
+
+            <p>{seed}</p>
+
+            <button onClick={generateSeed}>
+                Generate Seed
+            </button>
+
         </div>
     );
 }
