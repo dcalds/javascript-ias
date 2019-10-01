1173c7d5a88c6fa654ddeb83eb66cf0914128d6f
Danilo Caldas <danilo-caldas-0000@hotmail.com>
2019-10-01 15:40:37 -0300

    Finalizado desafio, LOADS, STORS funcionando

--- functions.js
+++ functions.js
@@ -14,9 +14,8 @@ M = {
     7: '0000000000000000000000000000000000000000',  // 7
     8: '0000000000000000000000000000000000000000',  // 8
     9: '0000000000000000000000000000000000000000',  // 9
-    10: '0000000000000000000000000000000000000000', // 10
-    11: '0000000000000000000000000000000000000001', // 11
-    12: '1000000000000000000000000000000000000001', // 12
+    10: '0000000000000000000000000000000000000001', // 10
+    11: '1000000000000000000000000000000000000001' // 11
 }
 
 // Unidade de Controle
@@ -34,31 +33,34 @@ MQ = '0'
 
 contadorDeCiclos = 1
 
-while (IR != '00000000') {
+
+while (IR != '00000000') { // Enquanto as instruções não forem 00000000 (Fim do programa), continua-se a buscar novas instruções
 
     console.log(`// ------------ Ciclo: ${contadorDeCiclos} ------------ //`)
 
     // Ciclo de Busca
-    if (IBR.length > 1) {
+    if (IBR.length > 1) {   // Caso haja alguma coisa no IBR significa que há instrução na direita para ser executada
 
-        IR = IBR.slice(0, 8)
-        MAR = IBR.slice(8, 20)
-        PC += 1
-        IBR = '0'
+        IR = IBR.slice(0, 8)    // Instrução da Direita
+        MAR = IBR.slice(8, 20)  // Endereço de Memória da Direita
+        PC += 1                 // PC vai apontar para a próxima instrução a ser executada
+        IBR = '0'               // IBR resetado, indicando que a instrução da direita foi executada
 
         console.log("DIREITA >>>")
+        console.log("PC :", PC)
     }
 
-    else {
+    else {                  // Caso não haja nenhuma instrução na direita (IBR vazio), é neste condicial onde separam-se os dois lados
 
-        MAR = PC
-        MBR = M[MAR]
+        MAR = PC                // PC passa o endereço para o MAR
+        MBR = M[MAR]            // MBR recebe o conteúdo armazenado no endereço de memória indicado pelo MAR
 
-        IR = MBR.slice(0, 8)
-        MAR = MBR.slice(8, 20)
-        IBR = MBR.slice(20, 40)
+        IR = MBR.slice(0, 8)    // IR recebe a instrução da esquerda
+        MAR = MBR.slice(8, 20)  // MAR recebe agora apenas o endereço de memória do lado esquerdo da instrução
+        IBR = MBR.slice(20, 40) // O IBR recebe o lado direito com OPCODE e Endereço de Memória, agora não está mais vazio
 
         console.log("ESQUERDA <<<")
+        console.log("PC :", PC)
     }
 
     // Ciclo de Execução
@@ -66,40 +68,47 @@ while (IR != '00000000') {
         // Transferência de Dados ---------------------------------------------------------- //
 
     if (IR === '00001010') {
-        AC = MQ        
+        AC = MQ
         console.log("LOAD MQ") // 1
+        console.log("AC :", AC)        
     }
 
     if (IR === '00001001') {
         MQ = M[binDec(MAR)]
-        console.log("LOAD MQ, M[X]") // 2 
+        console.log(`LOAD MQ, M[${binDec(MAR)}]`) // 2 
+        console.log("MQ :", MQ)        
     }
 
     if (IR === '00100001') {
         M[binDec(MAR)] = AC
-        console.log("STOR M[X]") // 3
+        console.log(`STOR M[${binDec(MAR)}]`) // 3
+        console.log("M[MAR] :", M[binDec(MAR)])
     }
 
     if (IR === '00000001') {
         
         AC = M[binDec(MAR)]
-        console.log("LOAD M[X]") // 4
+        console.log(`LOAD M[${binDec(MAR)}]`) // 4
+        console.log("AC :", AC)        
     }
     if (IR === '00000010') {
         
         ACpos = M[binDec(MAR)]
         AC = '1' + ACpos.slice(1,40)
-        console.log("LOAD -M[X]") // 5
+        console.log(`LOAD -M[${binDec(MAR)}]`) // 5
+        console.log("AC :", AC)        
     }
     if (IR === '00000011') {
         AC = M[binDec(MAR)]
-        console.log("LOAD |M[X]|") // 6
+        console.log(`LOAD |M[${binDec(MAR)}]|`) // 6
+        console.log("AC :", AC)        
     }
 
     if (IR === '00000100') {
         ACpos = M[binDec(MAR)]
         AC = '1' + ACpos.slice(1,40)
-        console.log("LOAD -|M[X]|") // 7
+        console.log(`LOAD -|M[${binDec(MAR)}]|`) // 7
+        console.log("AC :", AC)        
     }
 
         // Desvio Incondicional ---------------------------------------------------------- //
@@ -172,6 +181,7 @@ while (IR != '00000000') {
 
     if (IR === '00010010') {
 
+        
         console.log("STOR M[X, 8:19]") // 20
     }
 
